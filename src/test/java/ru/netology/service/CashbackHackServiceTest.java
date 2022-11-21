aab0b310549232c268131f87a7b5fd73b672cee0
Mikhail Kutsenkov <km36rus@gmail.com>
2022-11-21 17:22:45 +0300

    branch junit4 bag

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.service;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
@@ -10,8 +10,8 @@ public class CashbackHackServiceTest {
    public void shouldReturn100IfAmountIs900() {
         // подготовка данных
         CashbackHackService service = new CashbackHackService();
-        int amount = 2000;
-        int expected = 1000;
+        int amount = 900;
+        int expected = 100;
 
         // выполнение целевого действия
         int actual = service.remain(amount);
@@ -24,14 +24,25 @@ public class CashbackHackServiceTest {
    public void shouldReturn0IfAmountIs1000() {
         // подготовка данных
         CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int expected = 1000;
+        int amount = 1100;
+        int expected = 900;
 
         // выполнение целевого действия
         int actual = service.remain(amount);
 
         // сравнение ОР и ФР
         assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldBuyMoreFor1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
     }
 
 }
